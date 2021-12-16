1c8d4ed977c7bd7e9a7862ee14220d8a5e6057a9
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-16 20:13:34 +0300

    Update (4)itc_sl_list.cpp

--- (4)itc_sl_list.cpp
+++ (4)itc_sl_list.cpp
@@ -3,10 +3,12 @@
 int itc_sl_list(const vector <int>& mass)
 {
 	int a=0;
+	if (mass == vector<int>{ })
+		return 0;
 	for (int i=1; i<mass.size(); i++){
-            if (mass[i] > mass[i-1]){
-                a++;
-            }
+                if (mass[i] > mass[i-1]){
+                	a++;
+            	}
 	}
 	return a;
 }
