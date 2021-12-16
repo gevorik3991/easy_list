6174e06cbaa54a61586235cae3594063d164f9de
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-16 20:10:09 +0300

    Update (2) itc_even_parts_list.cpp

--- (2) itc_even_parts_list.cpp
+++ (2) itc_even_parts_list.cpp
@@ -2,11 +2,11 @@
 
 void itc_even_parts_list(const vector <int>& mass, vector <int>& mass2)
 {
-	for (int i=0; i < mass.size(); i++){
-	    if (mass == vector<int>{ })
+	if (mass == vector<int>{ })
 	      return ;
-        if (mass[i]%2 == 0){
-                mass2.push_back(mass[i]);
-        }
+	for (int i=0; i < mass.size(); i++){    
+        	if (mass[i]%2 == 0){
+                	mass2.push_back(mass[i]);
+        	}
 	}
 }
