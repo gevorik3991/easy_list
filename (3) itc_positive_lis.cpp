57dd5ba798fc85a3ad0578c5daf429e7d851eb57
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-16 20:12:30 +0300

    Update (3) itc_positive_lis.cpp

--- (3) itc_positive_lis.cpp
+++ (3) itc_positive_lis.cpp
@@ -3,13 +3,12 @@
 int itc_positive_list(const vector <int>& mass)
 {
 	int a=0;
-    if (mass == vector<int>{ })
-        return 0;
+        if (mass == vector<int>{ })
+		return 0;
 	for (int i=0; i < mass.size(); i++){
-
-        if (mass[i] > 0){
-                a++;
-        }
+        	if (mass[i] > 0){
+              	    a++;
+		}
 	}
 	return a;
 }
