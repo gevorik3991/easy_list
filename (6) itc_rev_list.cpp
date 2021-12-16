2cc83b3c12281b50e3caab7787959be01f567652
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-16 20:14:52 +0300

    Update (6) itc_rev_list.cpp

--- (6) itc_rev_list.cpp
+++ (6) itc_rev_list.cpp
@@ -3,7 +3,7 @@
 void itc_rev_list(vector <int>& mass)
 {
 	if (mass == vector<int>{ })
-        return ;
+        	return ;
 	int a = mass.size() - 1, b = 0, c;
 	while (a >= mass.size() / 2){
 		c = mass[b];
