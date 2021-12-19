8a826c8f924c96351e51a4a58f33930f07674966
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-19 17:13:24 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -13,6 +13,7 @@ bool itc_same_parts_list(const vector <int>& mass);
 void itc_rev_list(vector <int>& mass);
 void itc_rev_par_list(vector <int>& mass);
 void itc_rshift_list(vector <int>& mass);
+void itc_lshift_list(vector <int>& mass);
 void itc_super_shift_list(vector <int>& mass, int n);
 
 #endif // EASY_LIST_H_INCLUDED
