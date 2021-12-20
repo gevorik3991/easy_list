89f3508988063e3897261ff0101926d37e5a219b
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-20 11:23:33 +0300

    Update itc_rshift_list.cpp

--- itc_rshift_list.cpp
+++ itc_rshift_list.cpp
@@ -2,14 +2,11 @@
 void itc_rshift_list(vector <int> &mass)
 {
     int len = mass.size();
-    if (len != 0)
-    {
+    if (len != 0){
         vector <int> res(mass);
         res[0] = mass[len - 1];
         for (int i = 1; i < len; i++)
-        {
             res[i] = mass[i - 1];
-        }
         mass = res;
     }
 }
