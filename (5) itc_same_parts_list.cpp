d1b0176eae1b84091c4172fc76b13c70b346ec6d
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-20 11:13:11 +0300

    Update (5) itc_same_parts_list.cpp

--- (5) itc_same_parts_list.cpp
+++ (5) itc_same_parts_list.cpp
@@ -5,16 +5,13 @@ bool itc_same_parts_list(const vector <int> &mass){
     int b = 0;
     if(a != 0){
         for(int i = 0; i < a; i++){
-            if(mass[i] < 0 && mass[i + 1] < 0){
+            if(mass[i] < 0 && mass[i + 1] < 0)
                 b++;
-            }
-            if(mass[i] >= 0 && mass[i + 1] >= 0 && i + 1 < a){
+            if(mass[i] >= 0 && mass[i + 1] >= 0 && i + 1 < a)
                 b++;
-            }
         }
     }
-    if(b > 0){
+    if(b > 0)
         return true;
-    }
     return false;
 }
