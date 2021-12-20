432c278f3b28772403e493d5b665bf04c276829a
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-12-20 11:28:06 +0300

    Update itc_super_shift_list.cpp

--- itc_super_shift_list.cpp
+++ itc_super_shift_list.cpp
@@ -6,9 +6,10 @@ void itc_super_shift_list(vector <int>& mass, int n)
 	if (n < 0)
 	   n = mass.size()-1+(n%mass.size());
 	else
-        n = n % mass.size();
+            n = n % mass.size();
 	if (n == 0)
             return ;
 	for (int i = 0; i < n; i++)
             itc_rshift_list(mass);
+    }
 }
